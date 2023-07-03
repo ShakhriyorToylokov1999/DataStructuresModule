6599c4eb36a245c1c48c057dcfaee1e6debf5bc5
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-07-03 20:21:59 +0500

    All Task2 tests passed

--- Tasks/HybridFlowProcessor.cs
+++ Tasks/HybridFlowProcessor.cs
@@ -1,28 +1,90 @@
 ﻿using System;
+using System.Reflection;
 using Tasks.DoNotChange;
 
 namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        public int Length { get; set; } = 0;
+        private Node head;
+        private Node tail;
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            head.next = head;
+            head.next.prev = null;
+            head = head.next;
+
+            return head.data;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item);
+
+            if (tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                node.prev = tail;
+                tail.next = node;
+                tail = node;
+            }
+
+            Length++;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            tail.prev = tail;
+            tail.next = null;
+            tail = tail.prev;
+
+            return tail.data;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item);
+
+            if (tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                node.prev = tail;
+                tail.next = node;
+                tail = node;
+            }
+
+            Length++;
+        }
+
+        private class Node
+        {
+            public T data;
+            public Node prev;
+            public Node next;
+            public Node(T item)
+            {
+                data = item;
+            }
         }
     }
 }
